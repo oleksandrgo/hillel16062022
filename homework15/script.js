da0894ea12a43d4fbdb5af6cf277e34ea322c20a
Kravchuk2022 <akravchuk2014@gmail.com>
2022-10-08 23:15:58 +0300

    Homework 15

---
+++ homework15/script.js
@@ -0,0 +1,45 @@
+
+
+
+//№1
+/*let a = prompt('Введите значение a');
+console.log(typeof a);
+let b = prompt('Введите значение b');
+console.log(typeof b);
+function dev(a,b){
+return parseInt(a)/parseInt(b);
+}
+let devide = dev(a,b);
+
+console.log('Делится='+devide);
+console.log('Делится с остатком '+parseInt(a)%parseInt(b));*/
+
+
+
+//№2
+/*let str = " ";
+let star = "*";
+function tri(){
+for (let i = 0; i < 7; i++) {
+  str += star;
+  console.log(str);
+}
+}
+tri()*/
+
+//№3
+/*for (let x=0; x<100; x++){
+if (x%2 === 0){
+continue;}
+console.log (x);
+}*/
+
+
+//№4 
+/*for(n = 1000, y = 0; n >= 50; n/2, y++){
+  n /= 2;
+ }
+ console.log(n);
+ console.log(y);*/
+
+
