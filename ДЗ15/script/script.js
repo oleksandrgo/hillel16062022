7a3bd1bfd5b344fbbe2f6bccfb7afc38ab327f3e
249ricky <249ricky@gmail.com>
2022-10-09 13:57:20 +0300

    home work 15

---
+++ ДЗ15/script/script.js
@@ -0,0 +1,50 @@
+//Задача 1
+/*let a = 11;
+let b = 2;
+if(a % b === 0){
+	console.log("Делится:" +(a / b));} else {
+	console.log("Делится с остатком:" +(a % b));
+}*/
+
+//Задача 2
+/*let a = 5;
+let b = "ꙭ";
+let c = "";
+function drawTriangle (a,b) {
+	for (let d = 0; d < a; d++) {
+  c += b;
+  console.log(c);}
+}
+drawTriangle(5,"ꙭ")*/
+
+//Задача 3
+/*for (let a = 0; a <= 100; a++){
+	if (a % 2 != 0){
+		console.log(a);
+	}
+}*/
+
+//Задача 4
+//Вариант 1
+/*for (n = 1000, i = 0; n >= 50; n / 2, i++){
+	n /= 2;
+console.log(n);
+console.log("Iterator:" +i);
+}*/
+
+
+//Вариант 2
+/*let n = 1000;
+let i = 0;
+while (n / 2){
+	if (n < 50){
+		break;
+	}
+	console.log(n);
+	n = n / 2;
+	console.log("Iterator" + i)
+	i = i + 1;
+}*/
+
+
+
