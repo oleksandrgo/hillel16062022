df5d3c64d92957becaac54acde93547e448d9641
YuliaKor1 <korolevaj8@gmail.com>
2022-10-09 16:06:18 +0300

    Home Work 15

---
+++ ДЗ15/script/script.js
@@ -0,0 +1,48 @@
+
+//alert('Решаем задачи?');
+console.log('Добро пожаловать');
+
+//Задача 1
+
+let a = 15;
+let b = 7;
+if(a%b===0)
+	{console.log ("Делится:" + a/b);
+} 
+else
+	{console.log ("Делится с остатком:" + a%b);
+};
+
+
+//Задача 2//
+
+/*function drawTriangle(t,d){
+		let w = 0;
+		let p = d;
+		while(w<t){
+		console.log(p);
+		w++;
+		p=p+d;
+		}
+	}
+
+drawTriangle(5,"*");*/
+
+//Задача 3//
+
+/*for (let с = 0; с <= 100; с++){
+	if (с%2===0) {
+		continue;
+	}
+	console.log(с);}*/
+
+//Задача 4//
+
+/*let n = 1000;
+let k = 0;
+do { n = n/2;
+	k++;
+}
+while (n>50);
+console.log("Итоговое число " + n);
+console.log("Количество итераций " + k);*/
