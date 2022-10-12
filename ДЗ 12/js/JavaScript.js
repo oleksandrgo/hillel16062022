40eca15b29295717b04d23353fa28d6cc23d0355
Kravchuk2022 <akravchuk2014@gmail.com>
2022-10-12 20:50:51 +0300

    ДЗ в папке 12

--- ДЗ 12/js/JavaScript.js
+++ ДЗ 12/js/JavaScript.js
@@ -3,9 +3,9 @@
             var phone = document.getElementById("phone");
             var email = document.getElementById("email");
 
-            var nameReg = new RegExp("^[A-z][a-z]{1,19}$");
+            var nameReg = new RegExp("^[a-zA-Z]{2,20}$");
             var phoneReg = new RegExp("^380(50|63|66|67|68|91|92|93|97|96)[0-9]{7}$");
-            var emailReg = new RegExp("^[a-z]{6,12}@(gmail.com|ukr.net)$")
+            var emailReg = new RegExp("^[a-zA-Z]{6,12}@(gmail.com|ukr.net)$");
 
 
             if (!name.value.match(nameReg)) {
