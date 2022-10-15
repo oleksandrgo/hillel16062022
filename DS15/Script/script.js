06a31edc33f1ae42409f2732b7966fbf8ba66540
Khr1st1na <khr1st1nanarovenko1995@gmail.com>
2022-10-15 05:30:40 +0300

    DS16.RegExp

--- DS15/Script/script.js
+++ DS15/Script/script.js
@@ -1,4 +1,4 @@
-let a = 5;
+/*let a = 5;
 let b = 5;
 if (a%b===0) {
 	console.log("Делится " + a/b);
@@ -35,4 +35,24 @@ k++;
 }
 while (n>=50);
 console.log("Получится число " + n);
-console.log("Количество итераций " + k);
+console.log("Количество итераций " + k);*/
+
+let name = document.querySelector('#name');
+let phone = document.querySelector('#phone');
+let email = document.querySelector('#email');
+
+let btn = document.querySelector(".btn");
+btn.addEventListener('click',showAlert);
+
+let re1 = /^[a-zA-Z]{2,20}$/;
+let re3 = /^[\w]{6,12}(@gmail\.com|@ukr\.net)$/;
+let re2 = /^380[50|63|66|67|68|91|92|93|97|96][0-9]{7}$/;
+
+function showAlert(){
+	if (re1.test(name.value)&&re2.test(phone.value)&&re3.test(email.value)) {
+		alert("Мы услышим вас");
+	}else{
+		alert("Мы вас не слышим");
+	}
+}
+
