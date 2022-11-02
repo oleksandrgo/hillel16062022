21ad9c00dd1a0bfde300c7899d94f706ad6f5a69
249ricky <249ricky@gmail.com>
2022-11-02 16:35:05 +0200

    home work 18

---
+++ ДЗ18/DZ/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Enter the number 1, 2 or 3");
+        Scanner inputFigure = new Scanner(System.in);
+        int i = inputFigure.nextInt();*/
+        /*if (i == 1)
+            System.out.println("The user entered the number 1");
+        if (i == 2)
+            System.out.println("The user entered the number 2");
+        if (i == 3)
+            System.out.println("The user entered the number 3");
+        else
+            System.out.println("The user entered wrong number");
+    }
+}*/
+        /*switch (i) {
+            case 1:
+                System.out.println("The user entered the number 1");
+                break;
+            case 2:
+                System.out.println("The user entered the number 2");
+                break;
+            case 3:
+                System.out.println("The user entered the number 3");
+                break;
+            case 4:
+                System.out.println("The user entered wrong number");
+                break;
+        }
+    }}*/
+
+        /*for (int i = 5; i > 0; i--)
+            System.out.println(i);
+    }
+}*/
+
+        /*for (int i = 1; i <= 10; i++)
+            System.out.println("3*"+i+"="+3*i);
+    }
+}*/
+        /*double sum = 0;
+        for (double i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(+(sum / 100));
+    }
+}*/
