485008aa0ae584fed5ad890d1f465afd53168fb4
DimaMigalin3008 <migalin.dv@gmail.com>
2022-11-08 12:47:04 +0200

    ДЗ№20(Пересдача)

--- ДЗ№20/AutomationQa/src/test/java/space/forstudy/qaauto/MainTests.java
+++ ДЗ№20/AutomationQa/src/test/java/space/forstudy/qaauto/MainTests.java
@@ -1,21 +1,20 @@
 package space.forstudy.qaauto;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.After;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.Random;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.util.random.RandomGenerator;
 
 
 public class MainTests {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeClass
     public static void configuration() {
@@ -26,6 +25,7 @@ public class MainTests {
     @Before
     public void precondition() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,2);
     }
 
     public String createMail() {
@@ -40,51 +40,35 @@ public class MainTests {
     String mileageTwo = mileageOne + "0";
 
     @Test
-    public void test() throws Exception {
+    public void test() throws InterruptedException {
         driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
-        Thread.sleep(1000);
         driver.findElement(By.cssSelector(".btn-primary")).click();
-        Thread.sleep(1000);
         driver.findElement(By.id("signupName")).sendKeys(name, Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.id("signupLastName")).sendKeys(lastName, Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.id("signupEmail")).sendKeys(createMail(), Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.id("signupPassword")).sendKeys("Mdv30081983", Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.id("signupRepeatPassword")).sendKeys("Mdv30081983");
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Register\"]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[text()=\" Profile \"]")).click();
-        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class=\"btn btn-white btn-sidebar sidebar_btn -profile\"]"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("profile_name")));
 
-        String checkFullName = (driver.findElement(By.className("profile_name")).getText());
-        if (!checkFullName.equals(fullName)) throw new Exception("Incorect Full Name");
+        WebElement CheckFullName = driver.findElement(By.className("profile_name"));
+
+        String TestCheckFullName = (CheckFullName.getText());
+        final String ExpectedCheckFullName= fullName;
+        Assert.assertEquals("Error", ExpectedCheckFullName, TestCheckFullName);
 
         driver.findElement(By.xpath("//a[text()=\"Garage\"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Add car\"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.id("addCarMileage")).sendKeys(mileageOne, Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//button[text()=\"Add fuel expense\"]")).click();
-        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".car_add-expense.btn"))).click();
         driver.findElement(By.name("mileage")).sendKeys(mileageTwo, Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.name("liters")).sendKeys("100", Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.name("totalCost")).sendKeys("1000", Keys.TAB);
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//a[text()=\" Settings \"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Remove my account\"]")).click();
-        Thread.sleep(1000);
         driver.findElement(By.xpath("//button[text()=\"Remove\"]")).click();
     }
         @After
