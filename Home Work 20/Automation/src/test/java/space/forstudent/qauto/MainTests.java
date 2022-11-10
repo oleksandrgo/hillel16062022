7224a6c546fce12a364faaaecacd9014e2e02adb
grib-shorty <Shorty@laptop-100635.local>
2022-11-10 14:26:54 +0100

    HW 20.1

--- Home Work 20/Automation/src/test/java/space/forstudent/qauto/MainTests.java
+++ Home Work 20/Automation/src/test/java/space/forstudent/qauto/MainTests.java
@@ -30,10 +30,14 @@ public class MainTests {
 
     @Test
     public void test() throws InterruptedException {
+
+        String name = "Vlad";
+        String lastname = "Drakula";
+
         driver.get("https://guest:welcome2qauto@qauto.forstudy.space/");
         driver.findElement(By.cssSelector(".btn-primary")).click();
-        driver.findElement(By.id("signupName")).sendKeys("Vlad");
-        driver.findElement(By.id("signupLastName")).sendKeys("Drakula");
+        driver.findElement(By.id("signupName")).sendKeys(name);
+        driver.findElement(By.id("signupLastName")).sendKeys(lastname);
         driver.findElement(By.id("signupEmail")).sendKeys("registeredEmail",
                 Math.floor(Math.random()*666)+"@gmail.com");
         driver.findElement(By.id("signupPassword")).sendKeys("VC14561476d");
@@ -42,14 +46,22 @@ public class MainTests {
 wait.until (ExpectedConditions.visibilityOfElementLocated
         (By.xpath("//a[@class=\"btn btn-white btn-sidebar sidebar_btn -profile\"]"))).click();
 
-//Пройти в профайл и проверить, что там такие же name и lastName как при регистрации
 
+        //Пройти в профайл и проверить, что там такие же name и lastName как при регистрации
         Thread.sleep(2000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("profile_name")));
+
+        String nameAndLastNameToCheck = new String();
+        nameAndLastNameToCheck = name+" "+lastname;
+
         WebElement profileName = driver.findElement(By.className("display-4"));
-        System.out.println(profileName.getText());
+        String checkName = profileName.getText();
 
-//Добавить авто
+        Assert.assertEquals("Name and Last Name are not equals",checkName, nameAndLastNameToCheck);
 
+
+
+        //Добавить авто
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-white")))
                         .click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-primary")))
@@ -58,7 +70,6 @@ wait.until (ExpectedConditions.visibilityOfElementLocated
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()=\"Add\"]")))
                         .click();
 //Добавить expenses
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".car_add-expense.btn")))
                         .click();
         driver.findElement(By.id("addExpenseMileage")).clear();
@@ -68,7 +79,6 @@ wait.until (ExpectedConditions.visibilityOfElementLocated
         driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
 
         // Удалить Аккаунт
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".dropdown-toggle")))
                         .click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()=\"Settings\"]")))
